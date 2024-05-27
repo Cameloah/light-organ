b74356651adb12dd2b44b49fa7ac5323cd97edb2
Cameloah <koor.jones@gmail.com>
2024-05-27 18:49:58 +0200

    fixed github update and lib handling

--- include/version.h
+++ include/version.h
@@ -4,12 +4,12 @@
 
 #pragma once
 
-#define FW_VERSION_MAJOR                    1
+#define FW_VERSION_MAJOR                    0
 #define FW_VERSION_MINOR                    0
-#define FW_VERSION_PATCH                    0
+#define FW_VERSION_PATCH                    1
 
 // set the github user and source repo here "user/repo"
-#define GITHUB_REPO     "Cameloah/light_organ"
+#define GITHUB_REPO     "Cameloah/light-organ"
 // enter the name of the firmware bin file here. A <version> string will be replaced by the FW version
 #define GITHUB_FW_BIN   "light_organ_<version>.bin"
 // enter the name of the compiled filesystem binary here
